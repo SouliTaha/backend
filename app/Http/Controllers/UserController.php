24fe3ffed888a62e54ab21424f59361138e599ba
Souli Taha <souli.taha@gmail.com>
2021-04-22 13:50:38 +0100

    final commit + user update profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
 
 class UserController extends Controller
 {
-    //
+ public function  UserById($id){
+        $id = Auth::user()->id;
+        $user = User::find($id);
+        if(is_null($user)){
+            return response()->json(['message'=>'User Not Found'], 404);
+        }
+             return response()->json($user,200);
+    
+    }
+/**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function editUser(Request $request, $id)
+    {
+        $id = Auth::user()->id;
+        $user = User::find($id);
+       if(is_null($user)){
+           return response()->json(['message'=> 'user not found'], 404);
+       }
+        
+        $user->update($request->all());
+        return response($user,200);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,17 @@ Route::group(['middleware' => 'auth:api' ,
     Route::post('refresh', 'AuthController@refresh');    
     Route::post('ResetPassword','ResetPasswordController@sendEmail');
     Route::post('changepassword','ChangePasswordController@process');
+    //product crud 
     Route::get('productlist','ProductController@list');
     Route::get('product/{id}','ProductController@getProductById');
     Route::post('add','ProductController@create');
     Route::put('edit/{id}','ProductController@edit');
     Route::delete('delete/{id}','ProductController@destroy');
+    //edit user 
+    Route::put('editUser/{id}','UserController@editUser');
+    // get a specific user 
+    Route::get('user/{id}','UserController@UserById');
+    
    
 });
 Route::group([
